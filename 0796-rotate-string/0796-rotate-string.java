879e1be00264ab4a33fc4352093c28d2f1796f3e
Shourya-18 <shouryasikarwar08@gmail.com>
2026-09-06 09:42:03 +0530

    Time: 1 ms (98.36%), Space: 43.2 MB (82.46%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        int n = s.length();
+
+        for (int k = 0; k < n; k++) {
+
+            boolean match = true;
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt((k + i) % n) != goal.charAt(i)) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
